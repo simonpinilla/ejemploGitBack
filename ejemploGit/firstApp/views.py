ebb69348cfb145535533e7ee14ad62490c676791
simonpinilla <simon.pinilla10@gmail.com>
2023-09-04 16:21:48 -0300

    Rama 1 terminada

---
+++ ejemploGit/firstApp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def vista1(request):
+    html = """
+    <style>
+        /* Estilo del banner */
+        .banner {
+            background-color: #0074E4;
+            color: white;
+            text-align: center;
+            padding: 20px;
+        }
+
+        /* Estilo de la fuente */
+        .fuente-especial {
+            font-family: 'Courier New', Courier, monospace;
+            font-size: 24px;
+        }
+    </style>
+    <div class="banner">
+        <h1 class="fuente-especial">
+            Bienvenido!
+        </h1>
+    </div>
+    """
+    return HttpResponse(html)
